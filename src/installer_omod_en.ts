c6106de75d4cc2bc10a34fd8d820ea47f93949ca
Mikaël Capelle <capelle.mikael@gmail.com>
2024-06-09 12:29:08 +0200

    Refactoring of game features for better management. (#11)

--- src/installer_omod_en.ts
+++ src/installer_omod_en.ts
@@ -51,106 +51,106 @@
 <context>
     <name>OMODFrameworkWrapper</name>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="364"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="365"/>
         <source>%1 wants to change [%2] %3 from &quot;%4&quot; to &quot;%5&quot;</source>
         <extracomment>%1 is the mod name [%2] is the ini section name. %3 is the ini setting name. %4 is the value already in Oblivion.ini. %5 is the value the mod wants to set.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="373"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="374"/>
         <source>%1 wants to set [%2] %3 to &quot;%4&quot;</source>
         <extracomment>%1 is the mod name [%2] is the ini section name. %3 is the ini setting name. %5 is the value the mod wants to set.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="376"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="377"/>
         <source>Update INI?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="467"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="468"/>
         <source>%1 has data for %2, but Mod Organizer 2 doesn&apos;t know what to do with it yet. Please report this to the Mod Organizer 2 development team (ideally by sending us your interface log) as we didn&apos;t find any OMODs that actually did this, and we need to know that they exist.</source>
         <extracomment>%1 is the mod name %2 is the name of a field in the OMOD&apos;s return data Hopefully this message will never be seen by anyone, but if it is, they need to know to tell the Mod Organizer 2 dev team.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="470"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="471"/>
         <source>Mod Organizer 2 can&apos;t completely install this OMOD.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="552"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="553"/>
         <source>Activate mod?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="556"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="557"/>
         <source>%1 contains the OMOD %2. OMODs may have post-installation actions like activating ESPs. Would you like to enable the mod so this can happen now?</source>
         <extracomment>%1 is the left-pane mod name. %2 is the name from the metadata of an OMOD.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="586"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="587"/>
         <source>%1 wants to activate %2. Do you want to do so?</source>
         <extracomment>%1 is the mod name. %2 is the plugin name.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="588"/>
-        <location filename="OMODFrameworkWrapper.cpp" line="631"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="589"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="632"/>
         <source>Activate plugin?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="600"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="601"/>
         <source>OMOD wants to activate missing plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="600"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="601"/>
         <source>An OMOD wants to activate a missing plugin. This shouldn&apos;t be possible. Please report this to a MO2 developer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="629"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="630"/>
         <source>%1 installed %2, but doesn&apos;t activate it by default. Do you want to activate it anyway?</source>
         <extracomment>%1 is the mod name. %2 is the plugin name.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="643"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="644"/>
         <source>OMOD claimed to install missing plugin</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="643"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="644"/>
         <source>An OMOD has activation settings for a missing plugin. This shouldn&apos;t be possible. Please report this to a MO2 developer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="665"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="666"/>
         <source>Register BSAs</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="669"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="670"/>
         <source>%1 wants to register the following BSA archives, but Mod Organizer 2 can&apos;t do that yet due to technical limitations:&lt;ul&gt;&lt;li&gt;%2&lt;/li&gt;&lt;/ul&gt;For now, your options include adding the BSA names to &lt;code&gt;sResourceArchiveList&lt;/code&gt; in the game INI, creating a dummy ESP with the same name, or extracting the BSA, all of which have drawbacks.</source>
         <extracomment>%1 is the OMOD name &lt;ul&gt;&lt;li&gt;%2&lt;/li&gt;&lt;/ul&gt; becomes a list of BSA files</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="744"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="745"/>
         <source>Display Readme?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="746"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="747"/>
         <source>The Readme may explain installation options. Display it?&lt;br&gt;It will remain visible until you close it.</source>
         <extracomment>&lt;br&gt; is a line break. Translators can remove it if it makes things clearer.</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="OMODFrameworkWrapper.cpp" line="752"/>
+        <location filename="OMODFrameworkWrapper.cpp" line="753"/>
         <source>%1 Readme</source>
         <extracomment>%1 is the mod name</extracomment>
         <translation type="unfinished"></translation>
